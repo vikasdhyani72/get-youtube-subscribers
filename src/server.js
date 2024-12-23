63c84041faceb5e1bb6450ef6f837427d2e8e326
vikasdhyani72 <vikasdhyani72@gmail.com>
2024-12-23 19:52:02 +0530

    Updated server to serve static files from public folder

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const app = require('./app')
 const mongoose = require('mongoose')
 const swaggerUi = require('swagger-ui-express')
 const swaggerJsdoc = require('swagger-jsdoc')
+const path = require('path')
 
 const DATABASE_URL = process.env.DATABASE_URL
 const PORT = process.env.PORT || 3000
@@ -28,13 +29,13 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsdoc(swaggerOptions)
 
 // Connect to MongoDB
-mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (err) => console.log('Database connection error:', err))
 db.once('open', () => console.log('Connected to the database'))
+
+// Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Use Swagger UI for /api-docs route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))

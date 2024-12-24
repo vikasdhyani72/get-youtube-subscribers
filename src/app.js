c70f4a04cd8583382b2647779fbb72de9a478ea1
vikasdhyani72 <vikasdhyani72@gmail.com>
2024-12-24 23:00:29 +0530

    change path

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // Serve static files
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname, '/')))
 
 // API routes
 app.use('/subscribers', subscriberRoutes)

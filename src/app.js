804b4b49fe865a44ad7816bbfb67241223b68f34
vikasdhyani72 <vikasdhyani72@gmail.com>
2024-12-24 23:30:06 +0530

    Fix static file serving and update app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const path = require('path')
 const subscriberRoutes = require('./routes/subscribers')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
 // Middleware for parsing JSON
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '/')))
+app.use('/images', express.static(path.join(__dirname, '../images')))
 
 // API routes
 app.use('/subscribers', subscriberRoutes)

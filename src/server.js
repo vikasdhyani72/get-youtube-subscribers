52796d433df2877bd0befc528fd0ab7f7336111e
vikasdhyani72 <vikasdhyani72@gmail.com>
2024-12-24 22:29:10 +0530

    change with render url

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${PORT}`,
+        url: `https://get-youtube-subscribers-ij6z.onrender.com`,
       },
     ],
   },
